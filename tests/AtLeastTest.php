05c06ccbf76a0c47a2a132864c0bd0982ed617bf
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-01-07 02:07:57 +0700

    fix test

--- tests/AtLeastTest.php
+++ tests/AtLeastTest.php
@@ -35,4 +35,58 @@ final class AtLeastTest extends TestCase
             ],
         ];
     }
+
+    /**
+     * @dataProvider atLeastTwiceDataProvider
+     */
+    public function testAtLeastTwice(array $data, callable $callable, bool $expected): void
+    {
+        $this->assertSame(
+            $expected,
+            AtLeast::atLeastTwice($data, $callable)
+        );
+    }
+
+    public function atLeastTwiceDataProvider(): array
+    {
+        return [
+            [
+                [1, "1", 2],
+                static fn($datum): bool => $datum == 1,
+                true,
+            ],
+            [
+                [1, "1", 3],
+                static fn($datum): bool => $datum === 1,
+                false,
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider atLeastTimesDataProvider
+     */
+    public function testAtLeastTime(array $data, callable $callable, bool $expected): void
+    {
+        $this->assertSame(
+            $expected,
+            AtLeast::atLeastTimes($data, $callable, 3)
+        );
+    }
+
+    public function atLeastTimesDataProvider(): array
+    {
+        return [
+            [
+                [0, false, null],
+                static fn($datum): bool => ! $datum,
+                true,
+            ],
+            [
+                [1, false, null],
+                static fn($datum): bool => ! $datum,
+                false,
+            ],
+        ];
+    }
 }

f613637306c3b4289ab9f7fc9b0650a87965e441
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-01-08 12:34:45 +0700

    Add Finder::firstTwo(), lastTwo(), firstTimes(), lastTimes()

--- src/Finder.php
+++ src/Finder.php
@@ -23,6 +23,24 @@ final class Finder
      * @param mixed[]        $data
      * @param callable(mixed $datum): bool $callable
      */
+    public static function firstTwo(array $data, callable $callable): mixed
+    {
+        return self::locateFirst($data, $callable, false, 2);
+    }
+
+    /**
+     * @param mixed[]        $data
+     * @param callable(mixed $datum): bool $callable
+     */
+    public static function firstTimes(array $data, callable $callable, int $times): mixed
+    {
+        return self::locateFirst($data, $callable, false, $times);
+    }
+
+    /**
+     * @param mixed[]        $data
+     * @param callable(mixed $datum): bool $callable
+     */
     public static function last(array $data, callable $callable): mixed
     {
         return self::locateFirst($data, $callable, true);
@@ -32,12 +50,31 @@ final class Finder
      * @param mixed[]        $data
      * @param callable(mixed $datum): bool $callable
      */
-    private static function locateFirst(array $data, callable $callable, bool $flip = false): mixed
+    public static function lastTwo(array $data, callable $callable): mixed
+    {
+        return self::locateFirst($data, $callable, true, 2);
+    }
+
+    /**
+     * @param mixed[]        $data
+     * @param callable(mixed $datum): bool $callable
+     */
+    public static function lastTimes(array $data, callable $callable, int $times): mixed
+    {
+        return self::locateFirst($data, $callable, true, $times);
+    }
+
+    /**
+     * @param mixed[]        $data
+     * @param callable(mixed $datum): bool $callable
+     */
+    private static function locateFirst(array $data, callable $callable, bool $flip = false, int $maxCount = 1): mixed
     {
         if ($flip) {
             $data = array_reverse($data);
         }
 
+        $totalFound = 0;
         foreach ($data as $datum) {
             $isFound = $callable($datum);
 
@@ -45,6 +82,12 @@ final class Finder
             Assert::boolean($isFound);
 
             if (! $isFound) {
+                continue;
+            }
+
+            ++$totalFound;
+
+            if ($totalFound < $maxCount) {
                 continue;
             }
 

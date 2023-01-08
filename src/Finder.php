96667f404206ef885706a17b5c106afb18e031e7
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-01-09 02:10:00 +0700

    comment

--- src/Finder.php
+++ src/Finder.php
@@ -51,6 +51,9 @@ final class Finder
         // ensure data is array for end(), key(), current(), prev() usage
         Assert::isArray($data);
 
+        // Use end, key, current, prev usage instead of array_reverse() to avoid immediatelly got "Out of memory" on many data
+        // see https://3v4l.org/IHo2H vs https://3v4l.org/Wqejc
+
         // go to end of array
         end($data);
 

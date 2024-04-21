d6e5ebcf334b109132c998a08bd0ddac6572cd9b
Abdul Malik Ikhsan <samsonasik@gmail.com>
2024-04-22 06:44:36 +0700

    add Counter

---
+++ src/Counter.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ArrayLookup;
+
+use Traversable;
+
+use function count;
+use function is_array;
+use function iterator_count;
+
+class Counter
+{
+    /**
+     * @param array<int|string, mixed>|Traversable<int|string, mixed> $data
+     */
+    public static function count(iterable $data): int
+    {
+        // php 8.1 compat
+        if (is_array($data)) {
+            return count($data);
+        }
+
+        return iterator_count($data);
+    }
+}

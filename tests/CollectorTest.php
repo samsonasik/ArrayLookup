a9607b564b97cb6921c85de7a55c6b40837f4aa3
Abdul Malik Ikhsan <samsonasik@gmail.com>
2024-09-07 20:22:19 +0700

    add test

---
+++ tests/CollectorTest.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ArrayLookup\Tests;
+
+use ArrayLookup\Collector;
+use PHPUnit\Framework\TestCase;
+use stdClass;
+
+use function is_string;
+use function trim;
+
+final class CollectorTest extends TestCase
+{
+    public function testWithoutLimit(): void
+    {
+        $data = [
+            ' a ',
+            ' b ',
+            ' c ',
+            new stdClass(),
+        ];
+
+        $results = Collector::setUp($data)
+            ->when(fn (mixed $datum): bool => is_string($datum))
+            ->withTransform(fn (string $datum): string => trim($datum))
+            ->getResults();
+
+        $this->assertSame(['a', 'b', 'c'], $results);
+    }
+
+    public function testWithLimit(): void
+    {
+        $data = [
+            ' a ',
+            ' b ',
+            ' c ',
+            new stdClass(),
+        ];
+
+        $results = Collector::setUp($data)
+            ->when(fn (mixed $datum): bool => is_string($datum))
+            ->withTransform(fn (string $datum): string => trim($datum))
+            ->withLimit(1)
+            ->getResults();
+
+        $this->assertSame(['a'], $results);
+    }
+}
